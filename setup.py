9df54403a6cc03660654f0f0369a58b1932bc7a1
Chris Martin <ch.martin@gmail.com>
2014-07-08 18:18:01 -0400

    version 1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def test_requirements():
 
 setup(
     name='sockfilter',
-    version='1.1',
+    version='1.2',
     py_modules=['sockfilter'],
     description='Block socket creation by host/port.',
     long_description=long_description(),
